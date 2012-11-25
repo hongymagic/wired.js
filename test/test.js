7cb3097f41e433701257a0591bef734af197ce8f
hongymagic <davidhong.code@gmail.com>
2012-11-25 16:03:12 +1100

    adding some tests, wrote a few basic functions to support two test cases

--- test/test.js
+++ test/test.js
@@ -1,58 +1,59 @@
-// qunit + specit and we only write AAA tests nothing lower than AAA+ thanks.
-describe('wired tests', function () {
+// qunit – we only write AAA tests nothing lower than AAA+ thanks.
 
+(function () {
 	var selector = 'input[data-wired]';
 	var shortcut = 'a';
 	var document = window.document;
 	var elements;
 
-	before(function () {
-		wired(selector, shortcut);
-		elements = document.querySelectorAll(selector);
+	module('wired', {
+		setup: function () {
+			wired(selector, shortcut);
+			elements = document.querySelectorAll(selector);
+		},
+		teardown: function () {
+			elements = null;
+		},
 	});
 
-	after(function () {
-		unwire(selector, shortcut);
-		element = null;
+	test('should select first wired element when pressed for the first time on document', function () {
+			// assert – see what i did here
+			var first = elements[0];
+			var focused;
+
+			// act – trigger shortcut key on document
+			emulateKeyEvent(shortcut, document);
+
+			// assert
+			stop();
+			setTimeout(function () {
+				focused = document.activeElement;
+				assert.equal(focused, first);
+				assert.equal(focused.name, 'first-name');
+				start();
+			}, 15);
+
+			// it's still AAA
 	});
 
-	it('should select first wired element when pressed for first time without focus', function () {
-		// assert – see what i did here
-		var first = elements[0];
-		var focused = focusedElement();
-		assert(focused).is(empty);
-		assert(focused).not(first);
-
-		// act – trigger shortcut key on document
-		emulateKeyEvent(shortcut, document);
-
-		// assert
-		stop();
-		setTimeout(function () {
-			focused = focusedElement();
-			assert(focused).is(first);
-			start();
-		}, 15);
-
-		// it's still AAA
-	});
-
-	it('should advance to next wired element when activated', function () {
+	test('should advance to next wired element when activated', function () {
 		// arrange
-		var first = elements[1];
+		var current = elements[0];
+		var next = elements[1];
+		current.focus();
 
 		// act
-		emulateKeyEvent(shortcut, first);
+		emulateKeyEvent(shortcut, current);
 
 		// assert
 		stop();
 		setTimeout(function () {
-			assert(focusedElement()).to(eql, first);
+			assert.equal(document.activeElement, next);
+			assert.equal(document.activeElement.name, 'location');
 			start();
 		}, 15);
 	});
-
-});
+})();
 
 //
 // Test helpers
@@ -72,16 +73,6 @@ function emulateKeyEvent (key, element) {
 
 	trigger(keydown, element);
 	trigger(keyup, element);
-}
-
-// get currently focused element
-function focusedElement () {
-	throw new 'argh';
-}
-
-// focus on given element
-function focusElement (element) {
-	throw new 'argh';
 }
 
 // trigger a given event on the element. if element is empty, trigger it on
--- wired.js
+++ wired.js
@@ -1,1 +1,51 @@
-var wired = {};
+var wired = (function () {
+	"use strict";
+
+	function addEventHandler (element, event, handler) {
+		if (element.addEventListener) {
+			element.addEventListener(event, handler, false);
+		} else {
+			element.attachEVent('on' + event, function () { handler(window.event); });
+		}
+	}
+
+	function keyUpHandler (elements, shortcut, current) {
+
+		return function (event) {
+			var key = String.fromCharCode(event.keyCode);
+			var next;
+
+			if (key === shortcut) {
+				current +=1 ;
+				next = elements.item(current);
+			}
+
+			if (next && next.focus) {
+				next.focus();
+			}
+		};
+	}
+
+	function toObjectString (object) {
+		return Object.prototype.toString.call(object);
+	}
+
+	function forEach (list, handler) {
+		Array.prototype.forEach.call(list, handler);
+	}
+
+	return function (selector, shortcut) {
+		// This is not a live NodeList
+		var elements = document.querySelectorAll(selector);
+		// FIXME: Current element may be set to be active on load
+		var current = -1;
+		var handler = keyUpHandler(elements, shortcut, current);
+
+		forEach(elements, function (element) {
+			addEventHandler(element, 'keyup', handler);
+		});
+
+		// Listen for global events as well
+		addEventHandler(document, 'keyup', handler);
+	};
+}());
